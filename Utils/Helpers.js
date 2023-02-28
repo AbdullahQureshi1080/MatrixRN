f36e63466e6cd00e52ccaec209ea54c353e9b38d
Abdullah Qureshi <abdullah.centrox@gmail.com>
2023-02-28 13:02:08 +0500

    adding attachment-decrypters helpers

--- Utils/Helpers.js
+++ Utils/Helpers.js
@@ -13,3 +13,35 @@ export const decryptPassword = password => {
   console.log('DECODED', decode);
   return decode;
 };
+
+export const blobToBase64 = url => {
+  return new Promise(async (resolve, _) => {
+    // do a request to the blob uri
+    const response = await fetch(url);
+
+    console.log('Response', blob);
+
+    // response has a method called .blob() to get the blob file
+    const blob = await response.blob();
+
+    console.log('BLOB IS', blob);
+
+    // instantiate a file reader
+    const fileReader = new FileReader();
+
+    // read the file
+    fileReader.readAsDataURL(blob);
+
+    fileReader.onloadend = function () {
+      console.log('Result', fileReader.result);
+      resolve(fileReader.result); // Here is the base64 string
+    };
+  });
+};
+
+export function getBlobSafeMimeType(mimetype) {
+  // if (!ALLOWED_BLOB_MIMETYPES.includes(mimetype)) {
+  return 'application/octet-stream';
+  // }
+  // return mimetype;
+}
